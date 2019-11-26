8c81ee282ed4c14e2f11fb45909b4215217e50f2
xLeDocteurx <lenoirc.fr@gmail.com>
2019-11-26 11:04:28 +0100

    Add Editor component to github page

--- github-page/src/main.js
+++ github-page/src/main.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
