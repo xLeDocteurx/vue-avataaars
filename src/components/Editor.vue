195240cfc02fda553518a082da3d442307c7a14d
Callan <cerveauxallanjean@gmail.com>
2020-01-24 17:28:53 +0100

    Update : Remove all form select for one component

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -36,121 +36,15 @@
       </b-col>
 
       <b-col sm="12" md="8">
-
         <b-card v-if="isRandom == false" no-body class="mt-3">
           <b-tabs content-class="mt-3" pills card>
-            <b-tab title="Body & Background">
-              <b-form-group label="Skin Color">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedSkinColor" 
-                :options="options.skinColor" 
-                ></b-form-select>
-              </b-form-group>
-              <b-form-group label="Background Type">
-                <b-form-radio-group
-                size="sm"
-                v-model="selectedBackgroundType"
-                :options="options.backgroundType"
-                ></b-form-radio-group>
-              </b-form-group>
-              <b-form-group label="↳ Background Color">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedBackgroundColor" 
-                :options="options.backgroundColor" 
-                ></b-form-select>
-              </b-form-group>
-            </b-tab>
-            <b-tab title="Face">
-              <b-form-group label="Eyes Type">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedEyesType" 
-                :options="options.eyesType" 
-                ></b-form-select>
-              </b-form-group>
-              <b-form-group label="↳ Eyebrow Type">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedEyebrowType" 
-                :options="options.eyebrowType" 
-                ></b-form-select>
-              </b-form-group>
-              <b-form-group label="Mouth Type">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedMouthType" 
-                :options="options.mouthType" 
-                ></b-form-select>
-              </b-form-group>
-              <b-form-group label="Facial Hair Type">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedFacialHairType" 
-                :options="options.facialHairType" 
-                ></b-form-select>
-              </b-form-group>
-              <b-form-group label="↳ Facial Hair Color">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedFacialHairColor" 
-                :options="options.facialHairColor" 
-                ></b-form-select>
-              </b-form-group>
-            </b-tab>
-            <b-tab title="Accessories">
-              <b-form-group label="Accessories">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedAccessoriesType" 
-                :options="options.accessoriesType" 
-                ></b-form-select>
-              </b-form-group>
-            </b-tab>
-            <b-tab title="Hair or Top">
-              <b-form-group label="Top Type">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedTopType" 
-                :options="options.topType" 
-                ></b-form-select>
-              </b-form-group>
-              <b-form-group label="↳  Top Color">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedTopColor" 
-                :options="options.topColor" 
-                ></b-form-select>
-              </b-form-group>
-            </b-tab>
-            <b-tab title="Clothes">
-              <b-form-group label="Clothes Type">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedClothesType" 
-                :options="options.clothesType" 
-                ></b-form-select>
-              </b-form-group>
-              <b-form-group label="↳ Clothes Color">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedClothesColor" 
-                :options="options.clothesColor" 
-                ></b-form-select>
-              </b-form-group>
-              <!-- <b-form-group label="↳ !!! Clothes Graphic">
-                <b-form-select 
-                size="sm" 
-                v-model="selectedClothesGraphicType" 
-                :options="options.clothesGraphicsType" 
-                ></b-form-select>
-              </b-form-group> -->
-            </b-tab>
-  
-            <!-- <b-tab title="Dummy"> -->
-            <!-- </b-tab> -->
-  
+            <FormSelect
+              v-for="option in options"
+              :key="option.title"
+              :title="option.title"
+              :labels="option.labels"
+              @updated_option_value="updateOption"
+            />
           </b-tabs>
         </b-card>
 
