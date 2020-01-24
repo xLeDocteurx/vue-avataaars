ddc32511f842abc5a052fab2fa14daeeb6ce2e9f
Callan <cerveauxallanjean@gmail.com>
2020-01-24 18:05:24 +0100

    Feature : Component Select render all select element, and send event to FormSelect after select change

---
+++ src/components/Form/Select.vue
@@ -0,0 +1,46 @@
+<template>
+	<b-form-select 
+	size="sm"
+	:options="options"
+	v-model="selected.value"
+	@change="selectedValue"
+	></b-form-select>
+</template>
+<script>
+	/* eslint-disable */
+	export default {
+		name:"Select",
+		props: {
+			default: {
+				type: String,
+				require: false
+			},
+			options: {
+				type: Array,
+				required: true				
+			},
+			label_name: {
+				type: String,
+				require: true
+			}
+		},
+		data(){
+			return {
+				selected: {
+					label_name: "",
+					value: ""
+				},
+			}
+		},
+		mounted() {
+			this.selected.label_name = this.label_name
+			this.selected.value = this.default
+		},
+		methods: {
+			selectedValue() {
+				console.log(this.selected.label_name, this.selected.value)
+				this.$emit('change_input_value', this.selected)
+			}
+		}
+	}
+</script>
