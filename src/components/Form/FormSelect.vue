5df007b0113af9623efd98caa70f1597d3f54af3
Callan <cerveauxallanjean@gmail.com>
2020-01-24 17:52:18 +0100

    Feature : create component for all select

---
+++ src/components/Form/FormSelect.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+
+	export default {
+		name: 'FormSelect',
+		components: {
+			Select
+		},
+		props: {
+			title: {
+				type: String,
+				required: true
+			},
+			labels: {
+				type: Array,
+				required: true
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
